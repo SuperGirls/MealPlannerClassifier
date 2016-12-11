362411a391f656a8f0b1933702e533ce22237021
zulvaf <zz_vha91@yahoo.com>
2016-12-11 21:59:31 +0700

    Create Classifier with Jess

--- src/mealplannerclassifier/MealPlannerClassifier.java
+++ src/mealplannerclassifier/MealPlannerClassifier.java
@@ -10,6 +10,9 @@ package mealplannerclassifier;
  * @author chairuniaulianusapati
  */
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import jess.*;
 import java.util.Scanner;
 public class MealPlannerClassifier {
@@ -17,37 +20,85 @@ public class MealPlannerClassifier {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String option;
+    public static void main(String[] args) throws FileNotFoundException, JessException, IOException {
+        Scanner scanner = new Scanner(System.in);
+        String input;
         
         System.out.println("---- Welcome To Meal Planner Classifier ----");
+        Rete engine = new Rete();
+
+        FileReader file = new FileReader("rules/rule.clp");
+        Context context = engine.getGlobalContext();
         
-        do {
-            try {
-                Rete r = new Rete();
-                r.eval("(deftemplate point \"A 2D point\" (slot x) (slot y))");
-
-                Fact f = new Fact("point", r);
-                f.setSlotValue("x", new Value(37, RU.INTEGER));
-                f.setSlotValue("y", new Value(49, RU.INTEGER));
-                r.assertFact(f);
-
-                r.eval("(facts)");
-                r.eval("(defrule myrule (A) => (printout t \"A\" crlf))");
-                Defrule dr = (Defrule) r.findDefrule("myrule");
-                System.out.println(new PrettyPrinter(dr));
-                
-            } catch (JessException ex) {
-                System.err.println(ex);
+        Rete rete = new Rete();
+        
+        try {
+            //Parse file and add rules
+            Jesp parser = new Jesp(file, engine);
+            Object result = Funcall.TRUE;
+            while (!result.equals(Funcall.EOF)) {
+                result = parser.parseExpression(context, false);
+                if (result instanceof HasLHS) {
+                    HasLHS rule = (HasLHS) result;
+                    rete.addDefrule(rule);
+                 }
+                if(result instanceof Deftemplate){
+                    Deftemplate template = (Deftemplate) result;
+                    System.out.println(template.toString());
+                    rete.addDeftemplate(template);
+                }
+              
             }
             
+            Fact f = new Fact("meals", rete);
             
-            System.out.print("> Repeat?(y/n): ");
-            option = input.nextLine();
-        } while (option.equals("y"));
-        
-        System.out.println("... bye!");
+            System.out.println("Hungry? yes/no");
+            input= scanner.nextLine();
+            if(!input.equals(""))
+                f.setSlotValue("hungry", new Value(input, RU.STRING));
+            
+            System.out.println("Hurry? yes/no");
+            input= scanner.nextLine();
+            if(!input.equals(""))
+                f.setSlotValue("hurry", new Value(input, RU.STRING));
+            
+            System.out.println("Temperature? hot/cold");
+            input= scanner.nextLine();
+            if(!input.equals(""))
+                f.setSlotValue("temperature", new Value(input, RU.STRING));
+            
+            System.out.println("Allergy Chicken? yes/no");
+            input= scanner.nextLine();
+            if(!input.equals(""))
+                f.setSlotValue("allergy_chicken", new Value(input, RU.STRING));
+            
+            System.out.println("Allergy Egg? yes/no");
+            input= scanner.nextLine();
+            if(!input.equals(""))
+                f.setSlotValue("allergy_egg", new Value(input, RU.STRING));
+            
+            System.out.println("Allergy Dairy? yes/no");
+            input= scanner.nextLine();
+            if(!input.equals(""))
+                f.setSlotValue("allergy_dairy", new Value(input, RU.STRING));
+            
+            System.out.println("Diabetes? yes/no");
+            input= scanner.nextLine();
+            if(!input.equals(""))
+                f.setSlotValue("diabetes", new Value(input, RU.STRING));
+            
+            rete.assertFact(f);
+            
+            rete.eval("(rules)");
+            System.out.println();
+            rete.eval("(facts)");
+            System.out.println();
+            rete.eval("(run)");
+            
+        } finally {
+          file.close();
+        }
+
     }
     
 }
